0d64b530c75a7ccc1d552bf9b5551e71c1d1043b
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-10-28 20:35:26 +0700

    Update Settings.php

--- src/Parkour/utils/Settings.php
+++ src/Parkour/utils/Settings.php
@@ -6,5 +6,6 @@ class Settings {
     public static function someMethod() {
         $pluginInstance = Main::getInstance();
         // Implementasi logika lain
+        $pluginInstance->getLogger()->info("Beberapa metode dalam Settings dipanggil.");
     }
 }
