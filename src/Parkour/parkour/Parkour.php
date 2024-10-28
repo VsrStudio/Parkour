6b43565ec151286aff6e771eda5ef26c6f680f8b
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-10-28 19:33:59 +0700

    Create Parkour.php

---
+++ src/Parkour/parkour/Parkour.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Parkour\parkour;
+
+class Parkour {
+    private $checkpoints = [];
+
+    public function addCheckpoint($checkpoint) {
+        $this->checkpoints[] = $checkpoint;
+    }
+
+    // Metode lain untuk mengelola logika parkour
+}
