7fa8733f4036622b7e0cdb942d80f820f7277642
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-10-28 20:19:11 +0700

    Update ParkourCommand.php

--- src/Parkour/commands/ParkourCommand.php
+++ src/Parkour/commands/ParkourCommand.php
@@ -3,10 +3,9 @@ namespace Parkour\commands;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use Parkour\utils\Main; // Pastikan untuk mengimpor kelas Main
+use Parkour\utils\Main;
 
 class ParkourCommand extends Command {
-
     public function __construct() {
         parent::__construct("parkour", "Perintah Parkour", "/parkour", []);
         $this->setPermission("parkour.command");
@@ -18,53 +17,6 @@ class ParkourCommand extends Command {
             return true;
         }
 
-        switch ($args[0] ?? "") {
-            case "start":
-                $this->startParkour($sender);
-                break;
-            case "stop":
-                $this->stopParkour($sender);
-                break;
-            case "leaderboard":
-                $this->showLeaderboard($sender);
-                break;
-            case "settings":
-                $this->showSettings($sender);
-                break;
-            case "stats":
-                $this->showStats($sender);
-                break;
-            case "checkpoint":
-                $this->setCheckpoint($sender);
-                break;
-            default:
-                Main::getInstance()->getLogger()->info("Perintah tidak dikenal.");
-                break;
-        }
-        return true;
-    }
-
-    private function startParkour(Player $player) {
-        // Implementasi logika start parkour
-    }
-
-    private function stopParkour(Player $player) {
-        // Implementasi logika stop parkour
-    }
-
-    private function showLeaderboard(Player $player) {
-        // Implementasi logika leaderboard
-    }
-
-    private function showSettings(Player $player) {
-        // Implementasi logika settings
-    }
-
-    private function showStats(Player $player) {
-        // Implementasi logika stats
-    }
-
-    private function setCheckpoint(Player $player) {
-        // Implementasi logika checkpoint
+        // ... (kode lainnya)
     }
 }
