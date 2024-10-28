3de769592c1eb8cb78887ef688f43843cd8ae48b
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-10-28 20:01:25 +0700

    Update Main.php

--- src/Parkour/Main.php
+++ src/Parkour/Main.php
@@ -1,18 +1,38 @@
-<?php
-
-namespace Parkour;
+namespace Parkour\utils;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginManager;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
-class Main extends PluginBase {
+class Main extends PluginBase implements Listener {
+    private static $instance;
 
-    protected function onEnable(): void {
-        $this->getLogger()->info("Plugin Parkour diaktifkan!");
-        // Inisialisasi lainnya
+    public function onLoad(): void {
+        self::$instance = $this; // Menyimpan instance plugin
     }
 
-    // Tambahkan metode lain untuk menangani perintah dan logika
+    public function onEnable(): void {
+        $this->getLogger()->info("Parkour plugin enabled.");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this); // Mendaftarkan event listener
+    }
+
+    public function onDisable(): void {
+        $this->getLogger()->info("Parkour plugin disabled.");
+    }
+
+    public static function getInstance(): self {
+        return self::$instance; // Mengembalikan instance plugin
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $player->sendMessage("Selamat datang di Parkour! Gunakan perintah /parkour untuk mulai.");
+    }
+
+    public function getLogger() {
+        return $this->getLogger(); // Mengembalikan logger untuk mencatat pesan
+    }
 }
