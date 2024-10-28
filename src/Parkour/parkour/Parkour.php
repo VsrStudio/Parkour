c2f6d6cb52c660bea94a474314029d6d5afa397e
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-10-28 20:36:17 +0700

    Update Parkour.php

--- src/Parkour/parkour/Parkour.php
+++ src/Parkour/parkour/Parkour.php
@@ -1,12 +1,14 @@
 namespace Parkour\parkour;
 
 use pocketmine\math\Vector3;
+use Parkour\utils\Main;
 
 class Parkour {
     private array $checkpoints = [];
 
     public function addCheckpoint(Vector3 $position): void {
         $this->checkpoints[] = $position; // Menambahkan checkpoint baru
+        Main::getInstance()->getLogger()->info("Checkpoint ditambahkan di {$position}");
     }
 
     public function getCheckpoints(): array {
