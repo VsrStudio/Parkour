c18add1a9019be1d15b904c9705a85418e04513e
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-10-28 20:06:55 +0700

    Update Settings.php

--- src/Parkour/utils/Settings.php
+++ src/Parkour/utils/Settings.php
@@ -1,24 +1,8 @@
-<?php
-
 namespace Parkour\utils;
 
-use pocketmine\utils\Config;
-
 class Settings {
-    private $config;
-
-    public function __construct() {
-        $this->config = new Config(Main::getInstance()->getDataFolder() . "config.yml", Config::YAML);
-    }
-
-    public function setSetting(string $key, $value): void {
-        $currentSettings = $this->config->get("settings");
-        $currentSettings[$key] = $value;
-        $this->config->set("settings", $currentSettings);
-        $this->config->save();
-    }
-
-    public function getSetting(string $key) {
-        return $this->config->get("settings")[$key] ?? null;
+    public static function someMethod() {
+        $pluginInstance = Main::getInstance(); // Memastikan Anda menggunakan instance Main
+        // Implementasi logika lain
     }
 }
